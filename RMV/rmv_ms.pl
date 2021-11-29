8befd9674a928dbffd09205d0d843beabd5c5c0e
Rance DeLong <r.delong@opengroup.org>
2021-11-29 14:26:27 -0800

    changed initialization code and comments

--- RMV/rmv_ms.pl
+++ RMV/rmv_ms.pl
@@ -11,27 +11,39 @@
 %   provided by Monitor Creation to accompany the generated monitor
 %   imported by initialize_ms_configuration
 %
-monitor_id('NuRV/Monitor/Mid_00001'). % established when the Monitor Sensor is defined
-monitor_atoms([a1:eq(x,2),a2:lt(x,2),a3:lt(y,x),a4:leq(x,2)]). % list of atomID:atomExpr
-monitor_variables([s,t,u,v,w,x,y,z]). % list of SUS state variables potentially observable
-monitor_observables([w,x,y,z]).
-monitor_reportables([x,z]). % subset of monitor_observables
-monitor_triggers([x]). % setter on these vars causes MS Responder trigger
+%   monitor_id - established when the Monitor Sensor is defined
+%   monitor_atoms - list of atomID:atomExpr to be evaluated
+%   monitor_variables - all potentially observable SUS variables in the model
+%   monitor_observables - all SUS variables used in properties or reportable
+%   monitor_reportables - subset of monitor_observables to be reported in heartbeat
+%   monitor_triggers - variables for which setter should trigger responder
+%
+:- dynamic monitor_id/1, monitor_atoms/1, monitor_variables/1,
+        monitor_observables/1, monitor_reportables/1, monitor_triggers/1.
+
+monitor_id('').
+monitor_atoms([]).
+monitor_variables([]).
+monitor_observables([]).
+monitor_reportables([]).
+monitor_triggers([]).
+
+% MONITOR SENSOR functions exposed to the SUS
+%
+% ms_startup is called by the SUS when it begins execution
+ms_startup :- init.
+
+% ms_shutdown is called by the SUS when it is shutting down
+ms_shutdown :- shutdown.
+
+% ms_step is called by SUS logic to explicitly trigger the responder
+ms_step :- responder.
 
 % MS STARTUP/SHUTDOWN INITIALIZATION
 %
 
 :- dynamic ms_initialized/1.
 ms_initialized(false).
-
-% functions exposed to the SUS
-% called by the SUS when it begins execution
-ms_startup :- init.
-
-% called by the SUS when it is shutting down
-ms_shutdown :- shutdown.
-
-ms_step :- responder.
 
 % internal startup / shutdown functions
 %
@@ -52,8 +64,25 @@ init :-
 
 initialize_ms_configuration :-
         % this operation will import the MS configuration definitions:
-        % monitor_id, monitor_atoms, monitor_variables, monitor_observables, monitor_reportables, monitor triggers
-        % For now these appear as example definitions at the top of this file
+        % monitor_id, monitor_atoms, monitor_variables, monitor_observables,
+        % monitor_reportables, monitor triggers
+
+        % For now these ad hoc definitions appear here as example
+        retractall(monitor_id(_)), assert(monitor_id('NuRV/Monitor/Mid_00001')),
+        retractall(monitor_atoms(_)),
+        assert(monitor_atoms([a1:eq(x,2),a2:lt(x,2),a3:lt(y,x),a4:leq(x,2)])),
+        retractall(monitor_variables(_)), assert(monitor_variables([s,t,u,v,w,x,y,z])),
+        retractall(monitor_observables(_)), assert(monitor_observables([w,x,y,z])),
+        retractall(monitor_reportables(_)), assert(monitor_reportables([x,z])),
+        retractall(monitor_triggers(_)), assert(monitor_triggers([x])),
+        assert(sus_var(s, undefined)),
+        assert(sus_var(t, undefined)),
+        assert(sus_var(u, undefined)),
+        assert(sus_var(v, undefined)),
+        assert(sus_var(w, undefined)),
+        assert(sus_var(x, 1)),
+        assert(sus_var(y, 2)),
+        assert(sus_var(z, 3)),
         true.
 
 shutdown :-
@@ -64,6 +93,7 @@ re_init :- un_init, init.
 
 un_init :-
 	% ...
+        retractall(sus_var(_,_)),
         retractall(ms_initialized(_)), assert(ms_initialized(false)).
 
 % Monitor Sensor Responder
@@ -72,9 +102,9 @@ un_init :-
 responder :-
         monitor_id(Mid),
         monitor_atoms(As),
-        aT_list_constructor(As,ATs),
+        aT_list_constructor(As,ATl),
         or_list_constructor(ORl),
-        ms_heartbeat(Mid,ATs,ORl),
+        ms_heartbeat(Mid,ATl,ORl),
         true.
 
 aT_list_constructor(As,ATs) :-
@@ -130,7 +160,7 @@ o_setter(Ovar,Oval) :-
         retractall(sus_var(Ovar,_)), assert(sus_var(Ovar,Oval)),
         monitor_triggers(Triggers),
         (   member(Ovar,Triggers)
-        ->  responder
+        ->  responder % invoke ms_step if called from outside this module
         ;   true
         ).
 
@@ -142,11 +172,3 @@ o_getter(_,undefined).
 % simulation of the SUS variables
 %
 :- dynamic sus_var/2.
-sus_var(s, undefined).
-sus_var(t, undefined).
-sus_var(u, undefined).
-sus_var(v, undefined).
-sus_var(w, undefined).
-sus_var(x, 1).
-sus_var(y, 2).
-sus_var(z, 3).
